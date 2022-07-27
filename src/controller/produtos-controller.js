259a20fc81e30038dd832332c7ebf5610bce7b10
danbaziewicz <danxbazi@gmail.com>
2022-07-27 15:15:28 -0300

    feat: criada rota get para produto.

---
+++ src/controller/produtos-controller.js
@@ -0,0 +1,34 @@
+import ProdutosModel from "../model/produtos.js";
+//importar base de dados
+
+
+const produtoController = (app) => {
+    //rotas ('/produto')
+
+    //criação da instancia
+    const modelProdutos = new ProdutosModel()
+    try {
+        app.get('/produto', (req, res)=>{
+            db.all('SELECT * FROM PRODUTOS',(erro, linhas)=>{
+                console.log(linhas)
+                console.log(erro)
+            })
+    
+            const todosProdutos = modelProdutos.pegaProdutos
+            
+            res.json({
+                "produto" : todosProdutos,
+                "erro" : false
+            })
+        })
+    } catch (error) {
+        res.json(
+            {"msg" : error.message,
+             "erro" : true}
+        )
+    }
+    
+
+}
+
+export default produtoController
