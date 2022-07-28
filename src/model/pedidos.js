ebddb45a7d73d6d7d4159a758c6f052f52d678c6
Talyssonbs <talyssonbs@gmail.com>
2022-07-27 21:45:04 -0300

    Criada a entidade pedidos

---
+++ src/model/pedidos.js
@@ -0,0 +1,14 @@
+export default class Pedidos {
+    constructor(data_do_pedido, valor_final, forma_de_pagamento) {
+        this.id = id++
+        this.data_do_pedido = data_do_pedido
+        this.valor_final = valor_final
+        this.forma_de_pagamento = forma_de_pagamento
+    }
+
+    inserePedidos = (pedido) => {
+
+    }
+
+    pegaPedidos = (pedido) => {}
+}
