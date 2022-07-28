e276d637ca1558b4f5c67fd9c42b8ae483377747
igor-diego <igordiego237@gmail.com>
2022-07-27 22:51:03 -0300

    criei a entidade funcionarios

---
+++ src/model/funcionarios.js
@@ -0,0 +1,37 @@
+
+class Funcionarios {
+   constructor(nome, cpf, email, telefone,cargo, dataDeAdmissao, salario) {
+    this.nome = nome,
+    this.cpf = cpf,
+    this.email = email,
+    this.telefone = telefone,
+    this.cargo = cargo,
+    this.dataDeAdmissao = dataDeAdmissao,
+    this.salario = salario  
+   }
+
+   insereFuncionario = (funcionario) => {
+
+   }
+
+
+   mostraFuncionario = () => {
+
+   }
+
+
+   mostraUmFuncionario = (email) => {
+      
+   }
+
+
+   deletaFuncionario = (id) => {
+
+   }
+
+
+   atualizaFuncionario = () => {
+
+   }
+
+}
