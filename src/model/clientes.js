d8d4a5d42f4d6a4911670b9a641e5cce2e72e1ff
Liliana1609 <lily.webdev22@gmail.com>
2022-07-29 15:49:39 -0400

    Inserindo os métodos da entidade

--- src/model/clientes.js
+++ src/model/clientes.js
@@ -1,12 +1,38 @@
 let id = 0
 class ClientesModel {
-  constructor(id, nomeCliente, cpfCliente, emailCliente, senhaCliente, telefoneCliente){
-   this.id = id,
-   this.nomeCliente = nomeCliente,
-   this.cpfCliente = cpfCliente,
-   this.emailCliente = emailCliente ? this.validaEmail(emailCliente) : undefined
-   this.telefoneCliente = telefoneCliente,
-   this.senhaCliente = senhaCliente ? this.validaSenha(senhaCliente) : undefined
+    constructor(id, nomeCliente, cpfCliente, emailCliente, senhaCliente, telefoneCliente) {
+        this.id = id,
+        this.nomeCliente = nomeCliente,
+        this.cpfCliente = cpfCliente,
+        this.emailCliente = emailCliente ? this.validaEmail(emailCliente) : undefined
+        this.telefoneCliente = telefoneCliente,
+        this.senhaCliente = senhaCliente ? this.validaSenha(senhaCliente) : undefined
+    }
+
+    insereCliente = (cliente) => {
+        bd.cliente.push(cliente)
+
+    }
+
+
+    pegaCliente = () => {
+        return bd.cliente
+
+    }
+
+
+    pegaUmCliente = (emailCliente) => {
+        return bd.cliente.filter(e => e.emailCliente === emailCliente)
+
+    }
+
+
+    dellCliente = (id) => {
+
+    }
+
+
+    atualizaCliente = () => {
+
     }
 }
-
