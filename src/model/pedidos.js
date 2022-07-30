735587449fa62cd969a7af80d4261c349cac371f
Talyssonbs <talyssonbs@gmail.com>
2022-07-30 18:54:34 -0300

    Fiz o DAO, fiz a função que cria e popula a tabela PEDIDOS, sistema para conectar ao banco de dados e corrigido arquivo pedidos.js

--- src/model/pedidos.js
+++ src/model/pedidos.js
@@ -1,16 +1,52 @@
-export default class Pedidos {
-    constructor(data_do_pedido, valor_final, forma_de_pagamento) {
-        this.id = id++
-        this.data_do_pedido = data_do_pedido
-        this.valor_final = valor_final
-        this.forma_de_pagamento = forma_de_pagamento
-    }
+import daoPedidos from "../DAO/pedidosDAO.js"
 
-    inserePedidos = (pedido) => {
+const Pedidos = {
+    inserePedido: async (pedido) => {
 
-    }
+        return await daoPedidos.inserePedido(pedido)
+    },
 
-    pegaPedidos = (pedido) => {
+    pegaPedidos: async () => {
+        return await daoPedidos.pegaPedidos()
+    },
 
+    pedidoPeloId: async (pedidoId) => {
+        return await daoPedidos.pedidoPeloId(pedidoId)
+    },
+
+    pedidoPelaData: async (dataPedido) => {
+        return await daoPedidos.pedidoPelaData(dataPedido)
+    },
+
+    pedidoPelaHora: async (horaPedido) => {
+        return await daoPedidos.pedidoPelaHora(horaPedido)
+    },
+
+    pedidoPeloValor: async (valorPedido) => {
+        return await daoPedidos.pedidoPeloValor(valorPedido)
+    },
+    pedidoPelaFormaPagamento: async (formaPagamento) => {
+        return await daoPedidos.pedidoPelaFormaPagamento(formaPagamento)
+    },
+
+    deletarPedido: async (id) => {
+        return await daoPedidos.deletarPedido(id)
+    },
+
+    atualizaPedido: async (id_pedido, novoPedido) => {
+        const pedido = await Pedidos.pedidoPeloId(pedidoId)
+        if (produtoAtual) {
+            const pedidoAtualizado = {
+                "data_do_pedido": novoPedido.data_do_pedido || pedido.data_do_pedido,
+                "hora_do_pedido": novoPedido.hora_do_pedido || pedido.hora_do_pedido,
+                "valor_final": novoPedido.valor_final || pedido.valor_final,
+                "forma_de_pagamento": novoPedido.forma_de_pagamento || pedido.forma_de_pagamento
+            }
+            return await daoPedidos.atualizaPedido(id_pedido, pedidoAtualizado)
+        } else {
+            throw new Error("Pedido não existe")
+        }
     }
 }
+
+export default Pedidos
