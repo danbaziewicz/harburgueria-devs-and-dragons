0f83e1fd1b38cab7605ef117c92a9c2fcc3e4e83
Talyssonbs <talyssonbs@gmail.com>
2022-07-30 20:14:48 -0300

    GET, POST, DELETE e UPDATE criados na pedidosController.js

--- src/database/popular.js
+++ src/database/popular.js
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 sqlite3.verbose()
 
-const bd = new sqlite3.Database('devs-and-dragons.db');
+const db = new sqlite3.Database('devs-and-dragons.db');
 
 const PEDIDOS_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "PRODUTOS" (
