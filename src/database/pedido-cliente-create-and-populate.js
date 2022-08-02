0233d7cfe109ca14f3ddddac7038bf41709f75b7
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-01 22:47:45 -0300

    Fix: adicao de chave estrangeira

--- src/database/pedido-cliente-create-and-populate.js
+++ src/database/pedido-cliente-create-and-populate.js
@@ -8,15 +8,19 @@ CREATE TABLE IF NOT EXISTS "PEDIDO_CLIENTE" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "id_pedido" int, 
     "id_cliente" int,  
-    "produto" String VarChar(255)    
+    "produto" String VarChar(255)  
+    FOREIGN KEY(PEDIDO) REFERENCES id_pedido(id_pedido)
+    FOREIGN KEY(CLIENTE) REFERENCES id_cliente(id_cliente)  
 );`;
 const ADD_PEDIDO_CLIENTE_DATA = `
 INSERT INTO PEDIDO_CLIENTE (id,id_pedido, id_cliente, produto)
 VALUES
-    (1, 2, 4, 'refrigerante'),
-    (2, 2, 4, 'vinho'),
+    (1, 2, 4, 'Sprite lata 350ml'),
+    (2, 2, 4, 'Coca-Cola lata 350ml'),
     (3, 2, 4,'Hamburguer de Costela'),
     (4, 3, 1, 'Hamburguer Vegano')
+    (4, 3, 1, 'Vinho Dv Catena Malbec')
+
     `
     
     function criaTabelaPedidoCliente() {
