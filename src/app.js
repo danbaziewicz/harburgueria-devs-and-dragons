8263faadc09d2b902b9e765496d50f972df9077d
danbaziewicz <danxbazi@gmail.com>
2022-08-01 23:48:04 -0300

    feat: adição de chamada controller na app.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from 'express'
 import produtoController from './controller/produtos-controller.js'
+import clienteController from './controller/clientes-controller.js'
+import funcionariosController from './controller/funcionarios-controller.js'
+import pedidoClienteController from './controller/pedido-cliente-controller.js'
+import pedidosController from './controller/pedidos-controller.js'
+import fornecedorController from './controller/fornecedor-controller.js'
 
 const app = express()
 
 app.use(express.json())
 
 produtoController(app)
+clienteController(app)
+funcionariosController(app)
+pedidoClienteController(app)
+pedidosController(app)
+fornecedorController(app)
 
 export default app
