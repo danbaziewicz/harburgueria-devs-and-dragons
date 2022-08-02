a8a7933913a8a9f61b14ee199b012da38b361981
igor-diego <igordiego237@gmail.com>
2022-08-01 22:52:11 -0300

    correção no nome do banco de dados

--- src/database/funcionarios-create-and-populate.js
+++ src/database/funcionarios-create-and-populate.js
@@ -1,7 +1,7 @@
 import sqlite3 from 'sqlite3';
 sqlite3.verbose()
 
-const db = new sqlite3.Database('dev-and-drangons.db');
+const db = new sqlite3.Database('devs-and-drangons.db');
 
 const FUNCIONARIOS_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "FUNCIONARIOS" (
