bb0a27e7cf30d0ac9d8c38aaf85fe701007b9436
danbaziewicz <danxbazi@gmail.com>
2022-08-04 15:50:47 -0300

    feat: adicionada criação de entidade e atributos fornecedores.

--- src/database/create-populate.js
+++ src/database/create-populate.js
@@ -102,8 +102,6 @@ CREATE TABLE IF NOT EXISTS "PEDIDO_CLIENTE" (
     "id_cliente" int,  
     "produto" text,
     "quantidade" int
-    
-    
 );`;
 
 const ADD_PEDIDO_CLIENTE_DATA = `
@@ -115,6 +113,28 @@ VALUES
     (4, 3, 1, 'Hamburguer Vegano',1),
     (5, 3, 1, 'Vinho Dv Catena Malbec',2)
     `
+const FORNECEDORES_SCHEMA = `
+CREATE TABLE IF NOT EXISTS "FORNECEDORES" (
+    "id_fornecedor" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "nome_fornecedor" text, 
+    "cnpj_fornecedor" text, 
+    "email_fornecedor" text, 
+    "cidade_fornecedor" text, 
+    "endereço_fornecedor" text,
+    "produto_fornecedor" text
+    );`;
+    
+const ADD_FORNECEDORES_DATA = `
+INSERT INTO FORNECEDORES (id_fornecedor, nome_fornecedor, cnpj_fornecedor, email_fornecedor, cidade_fornecedor, endereço_fornecedor, produto_fornecedor)
+VALUES
+    (1, 'José', '53.948.343/0001-49', 'josel79@gmail.com', 'Rio de Janeiro', '4243 Desiree Gateway Suite 854 - Concord, VT / 68035', 'papel higiênico'),
+    (2, 'Carlos', '69.886.035/0001-76', 'carlos96@hotmail.com', 'Nova Iguaçu', '224 Pietro Crescent Suite 291 - Folsom, MA / 87559', 'pão brioche'/),
+    (3, 'Carla', '89.829.020/0001-83', 'fabricio_dicki84@yahoo.com', 'Nitoroi', '391 Wilber Cape Apt. 510 - Littleton, FL / 94156', 'molhos'),
+    (4, 'Leandro', '66.512.970/0001-84', 'leandro21@hotmail.com', 'Campo grande', '129 Koch Extensions Suite 247 - Overland Park, OR / 17251', 'bebidas'),
+    (5, 'Pedro', '70.162.115/0001-68', 'pedro77@yahoo.com', 'Caxias', '48372 Lucas Creek Suite 378 - Mankato, AK / 10925', 'produtos de limpeza'),
+    (6, 'Paulo', '52.790.304/0001-01', 'paulo.stroman58@yahoo.com', 'Madureira', '3016 Lind Island Apt. 008 - Columbus, WY / 24337', 'embalagem'),
+    (7, 'Janaina' , '50.540.372/0001-89', 'thania3026@uorak.com', 'Volta Redonda' , 'Rua Autazes 661', 'molhos')
+`
 
 function criaTabelaClientes() {
   db.run(CLIENTES_SCHEMA, (error)=>{
@@ -196,6 +216,22 @@ function populaTabelaPedidoCliente() {
     })
 }
 
+function criaTabelaFornecedores() {
+    db.run(FORNECEDORES_SCHEMA, (error)=>{
+        if(error){
+            console.log("Erro ao criar a tabela FORNECEDORES")
+        }
+    })
+}
+
+function populaTabelaFornecedores() {
+    db.run(ADD_FORNECEDORES_DATA, (error)=>{
+        if(error){
+            console.log("Erro ao popular a tabela FORNECEDORES")
+        }
+    })
+}
+
 db.serialize(()=>{
     criaTabelaClientes()
     populaTabelaClientes()
@@ -207,4 +243,6 @@ db.serialize(()=>{
     populaTabelaProdutos()
     criaTabelaPedidoCliente()
     populaTabelaPedidoCliente()
+    criaTabelaFornecedores()
+    populaTabelaFornecedores()
 })
