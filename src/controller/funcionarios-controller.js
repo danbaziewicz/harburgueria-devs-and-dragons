639c4f264e642a50c38698d016dcafc5af925599
Talyssonbs <talyssonbs@gmail.com>
2022-08-05 19:52:02 -0300

    Rota / inserida

--- src/controller/funcionarios-controller.js
+++ src/controller/funcionarios-controller.js
@@ -1,19 +1,58 @@
 import funcionariosModel from '../model/funcionarios-model.js'
-import {criaFuncionarios} from '../services/valida-funcionario.js' 
+import {
+    criaFuncionarios
+} from '../services/valida-funcionario.js'
 
 
 const funcionariosController = (app) => {
+    app.get('/', (req, res) => {
+        res.send(`
+        <div style="padding-left: 0.938rem; padding-right: 0.938rem;">
+
+            <h1>Bem vindo(a) à API - Hamburgueria Devs and Dragons </h1>
+
+            <ul>
+
+                <li  style="font-size: 1.25rem;">Para ver os clientes, basta por /cliente após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os produtos, basta por /produto após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os funcionários, basta por /funcionarios após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os pedidos, basta por /pedido após o fim da url.</li>
+                <br>
+
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os pedido-cliente, basta por /pedido-cliente após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os fornecedores, basta por /fornecedor após o fim da url.</li>
+                <br>
+
+
+            </ul>
+
+            <br>
+
+            <p><a href="https://github.com/danbaziewicz/harburgueria-devs-and-dragons" target="_blank">Repositório da api</a></p>
+
+            <br>
+
+            <p>Api criada por: Squad 2 - Resília</p>
+
+        </div>
+        `)
+    })
     app.get('/funcionarios', async (req, res) => {
         try {
             const todosFuncionarios = await funcionariosModel.mostrafuncionarios()
             res.json({
                 "Funcionarios": todosFuncionarios,
-                "erro": false        
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "Msg" : erro.message,
-                "erro" : true
+                "Msg": erro.message,
+                "erro": true
             })
         }
     })
@@ -24,13 +63,13 @@ const funcionariosController = (app) => {
         try {
             const funcionario = await funcionariosModel.mostraUmFuncionario(idFuncionarios)
             res.json({
-                "Funcionario" : funcionario,
-                "erro" : false
+                "Funcionario": funcionario,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "Msg" : erro.message,
-                "erro" : true
+                "Msg": erro.message,
+                "erro": true
             })
         }
     })
@@ -43,13 +82,13 @@ const funcionariosController = (app) => {
             await funcionariosModel.insereFuncionarios(funcionarioNovo)
             res.json({
                 "msg": "Funcionário cadastrado com sucesso",
-                "Funcionarios" : funcionarioNovo,
-                "erro" : false
+                "Funcionarios": funcionarioNovo,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
@@ -59,32 +98,32 @@ const funcionariosController = (app) => {
         const id = req.params.id
         try {
             const novosFuncionario = criaFuncionarios(body.nome, body.cpf, body.email, body.telefone, body.cargo, body.dataDeAdmissao, body.salario)
-            await funcionariosModel.atualizaFuncionarios(id, novosFuncionario) 
+            await funcionariosModel.atualizaFuncionarios(id, novosFuncionario)
             res.json({
                 "msg": `Funcionario ${id} atualizado com sucesso`,
-                "Funcionario" : novosFuncionario,
-                "erro" : false
+                "Funcionario": novosFuncionario,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
 
-    app.delete('/funcionarios/id/:id', async (req,res) => {
+    app.delete('/funcionarios/id/:id', async (req, res) => {
         const id = req.params.id
         try {
             await funcionariosModel.deletaFuncionarios(id)
             res.json({
-                "Msg" : `Funcionario ${id} deletado com sucesso`,
-                "erro" : false
+                "Msg": `Funcionario ${id} deletado com sucesso`,
+                "erro": false
             })
         } catch (error) {
             res.json({
-                "msg" : error.message,
-                "erro" : true
+                "msg": error.message,
+                "erro": true
             })
         }
     })
--- src/controller/pedido-cliente-controller.js
+++ src/controller/pedido-cliente-controller.js
@@ -1,138 +1,177 @@
 import pedidoClienteModel from "../model/pedido-cliente-model.js";
-import { criaPedidoCliente } from "../services/valida-pedido-cliente.js";
+import {
+    criaPedidoCliente
+} from "../services/valida-pedido-cliente.js";
 
 const pedidoClienteController = (app) => {
 
-    app.get('/pedido-cliente', async (req, res)=>{
+    app.get('/', (req, res) => {
+        res.send(`
+        <div style="padding-left: 0.938rem; padding-right: 0.938rem;">
+
+            <h1>Bem vindo(a) à API - Hamburgueria Devs and Dragons </h1>
+
+            <ul>
+
+                <li  style="font-size: 1.25rem;">Para ver os clientes, basta por /cliente após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os produtos, basta por /produto após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os funcionários, basta por /funcionarios após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os pedidos, basta por /pedido após o fim da url.</li>
+                <br>
+
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os pedido-cliente, basta por /pedido-cliente após o fim da url.</li>
+                <br>
+                <li  style="font-size: 1.25rem;">Para ver os fornecedores, basta por /fornecedor após o fim da url.</li>
+                <br>
+
+
+            </ul>
+
+            <br>
+
+            <p><a href="https://github.com/danbaziewicz/harburgueria-devs-and-dragons" target="_blank">Repositório da api</a></p>
+
+            <br>
+
+            <p>Api criada por: Squad 2 - Resília</p>
+
+        </div>
+        `)
+    })
+    app.get('/pedido-cliente', async (req, res) => {
         try {
             const todosPedidoCliente = await pedidoClienteModel.pegaPedidoCliente()
             res.json({
-                "pedido_cliente" : todosPedidoCliente,
-                "erro" : false
+                "pedido_cliente": todosPedidoCliente,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
 
-    app.get('/pedido-cliente/id/:id', async (req, res)=>{
+    app.get('/pedido-cliente/id/:id', async (req, res) => {
         const pedidoClienteId = req.params.id
         try {
             const pedidoCliente = await pedidoClienteModel.pegaPedidoClienteById(pedidoClienteId)
             res.json({
-                "Pedido Cliente" : pedidoCliente,
-                "erro" : false
+                "Pedido Cliente": pedidoCliente,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
 
-    app.get('/pedido-cliente/idpedido/:id_pedido', async (req, res)=>{
+    app.get('/pedido-cliente/idpedido/:id_pedido', async (req, res) => {
         const pedidoClienteIdPedido = req.params.id_pedido
         try {
             const IdPedido = await pedidoClienteModel.pegaPedidoClienteByIdPedido(pedidoClienteIdPedido)
             res.json({
-                "id_pedido" : IdPedido,
-                "erro" : false
+                "id_pedido": IdPedido,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
 
-    app.get('/pedido-cliente/idcliente/:id_cliente', async (req, res)=>{
+    app.get('/pedido-cliente/idcliente/:id_cliente', async (req, res) => {
         const pedidoClienteIdCliente = req.params.id_cliente
         try {
             const idCliente = await pedidoClienteModel.pegaPedidoClienteByIdCliente(pedidoClienteIdCliente)
             res.json({
-                "id_cliente" : idCliente,
-                "erro" : false
+                "id_cliente": idCliente,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
 
-    app.get('/pedido-cliente/produto/:produto', async (req, res)=>{
+    app.get('/pedido-cliente/produto/:produto', async (req, res) => {
         const pedidoClienteProduto = req.params.produto
         try {
             const produto = await pedidoClienteModel.pegaPedidoClienteByProduto(pedidoClienteProduto)
             res.json({
-                "produtos" : produto,
-                "erro" : false
+                "produtos": produto,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
 
-    app.post('/pedido-cliente', async (req, res)=>{
+    app.post('/pedido-cliente', async (req, res) => {
         const body = req.body
         try {
-            const novoPedidoCliente = criaPedidoCliente(body.id_pedido, body.id_cliente, body.produto,body.quantidade);
+            const novoPedidoCliente = criaPedidoCliente(body.id_pedido, body.id_cliente, body.produto, body.quantidade);
             await pedidoClienteModel.inserePedidoCliente(novoPedidoCliente)
             res.json({
-                "msg" : "Pedido cliente inserido com sucesso",
-                "pedido-cliente" : novoPedidoCliente,
-                "erro" : false
+                "msg": "Pedido cliente inserido com sucesso",
+                "pedido-cliente": novoPedidoCliente,
+                "erro": false
             })
         } catch (erro) {
             res.json({
-                "msg" : erro.message,
-                "erro" : true
+                "msg": erro.message,
+                "erro": true
             })
         }
     })
 
-    app.delete('/pedido-cliente/id/:id', async (req, res)=>{
+    app.delete('/pedido-cliente/id/:id', async (req, res) => {
         const id = req.params.id
         try {
             await pedidoClienteModel.deletaPedidoCliente(id)
 
-            res.json(
-                {"msg" : `Pedido cliente ${id} deletado com sucesso`,
-                "erro" : false}
-            )
+            res.json({
+                "msg": `Pedido cliente ${id} deletado com sucesso`,
+                "erro": false
+            })
         } catch (error) {
-            res.json(
-                {"msg" : error.message,
-                 "erro" : true}
-            )
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
         }
     })
-    
-    app.put('/pedido-cliente/id/:id', async (req, res)=>{
+
+    app.put('/pedido-cliente/id/:id', async (req, res) => {
         const body = req.body
         const id = req.params.id
         try {
-            const novoPedidoCliente = criaPedidoCliente(body.id_pedido, body.id_cliente, body.produto,body.quantidade);
+            const novoPedidoCliente = criaPedidoCliente(body.id_pedido, body.id_cliente, body.produto, body.quantidade);
             await pedidoClienteModel.atualizaPedidoCliente(id, novoPedidoCliente)
             res.json({
-                "msg" : "Pedido cliente atualizado com sucesso",
-                "pedido-cliente" : novoPedidoCliente,
-                "erro" : false
+                "msg": "Pedido cliente atualizado com sucesso",
+                "pedido-cliente": novoPedidoCliente,
+                "erro": false
             })
         } catch (error) {
-            res.json(
-                {"msg" : error.message,
-                 "erro" : true}
-            )
+            res.json({
+                "msg": error.message,
+                "erro": true
+            })
         }
     })
 }
